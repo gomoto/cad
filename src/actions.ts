b958b77f079dadfbabf81c165c779db00b0ef33d
Ryan Gomoto <ryan@cyrusbio.com>
2018-07-01 15:54:51 -0700

    Action creators must return plain objects, not classes

--- src/actions.ts
+++ src/actions.ts
@@ -2,13 +2,17 @@ import * as Redux from 'redux';
 
 export const LOAD_FILES = 'load_files';
 
-export class LoadFilesAction implements Redux.Action {
-  type = LOAD_FILES;
-  constructor(public files: File[]) {}
+export interface LoadFilesAction extends Redux.Action {
+  type: typeof LOAD_FILES;
+  files: File[];
 }
 
+// NOTE: action creators must return plain objects, not classes
 export function loadFile(files: File[]): LoadFilesAction {
-  return new LoadFilesAction(files);
+  return {
+    type: LOAD_FILES,
+    files
+  };
 }
 
 // Composite action
